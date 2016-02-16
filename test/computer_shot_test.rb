5d77d49d4a068848fd51e6ea6bffdeed12a15378
marinacor1 <marinaspam1234@gmail.com>
2016-02-16 14:35:33 -0700

    adding computer shot and player feedback tests

--- test/computer_shot_test.rb
+++ test/computer_shot_test.rb
@@ -18,4 +18,13 @@ class ComputerShotTest < Minitest::Test
 
   def test_computer_shot_is_random
   end
+
+  def test_after_computer_plays_shows_info_about_play
+  end
+
+  def test_after_computer_plays_shows_map
+  end
+
+  def test_at_end_of_computer_game_goes_to_player_shot
+  end
 end
