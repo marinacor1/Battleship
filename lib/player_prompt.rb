d6c5a6d9cf50a8c1d8dad3d14b12018965ff7662
marinacor1 <marinaspam1234@gmail.com>
2016-02-16 19:40:55 -0700

    initial coordiate structure built

--- lib/player_prompt.rb
+++ lib/player_prompt.rb
@@ -5,10 +5,10 @@ class PlayerPrompt
   def initialize
     @error = false
     @possible_responses = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
-    @a = [nil, nil, nil, nil]
-    @b = [nil, nil, nil, nil]
-    @c = [nil, nil, nil, nil]
-    @d = [nil, nil, nil, nil]
+    @a = ["a:", nil, nil, nil, nil]
+    @b = ["b:", nil, nil, nil, nil]
+    @c = ["c:", nil, nil, nil, nil]
+    @d = ["d:", nil, nil, nil, nil]
   end
 
   def player_initial_ship_setup
@@ -34,14 +34,19 @@ class PlayerPrompt
   def coordinate_setup(input)
     input_array = input.upcase.split
     input_array.each do |coordinate|
+      # binding.pry
     if coordinate.start_with?("A")
       @a.insert(coordinate[1].to_i, "X")
+      @a.delete_at(coordinate[1] + 1)
     elsif coordinate.start_with?("B")
       @b.insert(coordinate[1].to_i, "X")
+      @b.delete_at(coordinate[1] + 1)
     elsif coordinate.start_with?("C")
       @c.insert(coordinate[1].to_i, "X")
+      @c.delete_at(coordinate[1] + 1)
     else
       @d.insert(coordinate[1].to_i, "X")
+      @d.delete_at(coordinate[1] + 1)
     end
   end
 
