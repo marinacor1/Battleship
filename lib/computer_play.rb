2312421648e9976bc168e1c7217606c2f3f1c2ec
marinacor1 <marinaspam1234@gmail.com>
2016-02-16 22:18:03 -0700

    lots of debugging needed for computer play

--- lib/computer_play.rb
+++ lib/computer_play.rb
@@ -10,59 +10,75 @@ class ComputerPlay
 
   def initial_computer_ship_setup
     small_ship_placement
-    binding.pry
     large_ship_placement
     @guess
   end
 
   def small_ship_placement
     random_row = rand(0..3)
-    @guess << @possible_responses[random_row].shuffle.pop
+    @guess << @possible_responses[random_row].shuffle!.pop
     coordinate_place = @possible_responses[random_row].index(@guess[0])
     letter_row = @guess[0][0]
     if coordinate_place < 3
-      @guess << @possible_responses[random_row][coordinate_place + 1]
-      @possible_responses[random_row].delete_at(coordinate_place + 1)
+      small_horizontal_placement(coordinate_place, letter_row, random_row)
     else
-      partner_place = ship_two_coordinate_place + 1
-      if letter_row == "A"
-        guess << "B#{partner_place}"
-      elsif letter_row == "B"
-        guess << "C#{partner_place}"
-      elsif letter_row == "C"
-        guess << "B#{partner_place}"
-      else
-        guess << "C#{partner_place}"
-      end
+      small_vertical_placement(coordinate_place, letter_row)
+    end
+  end
+
+  def small_horizontal_placement(coordinate_place, letter_row, random_row)
+    @guess << @possible_responses[random_row][coordinate_place + 1]
+    @possible_responses[random_row].delete_at(coordinate_place + 1)
+  end
+
+  def small_vertical_placement(coordinate_place, letter_row)
+    partner_place = coordinate_place + 1
+    if letter_row == "A"
+      guess << "B#{partner_place}"
+    elsif letter_row == "B"
+      guess << "C#{partner_place}"
+    elsif letter_row == "C"
+      guess << "B#{partner_place}"
+    else
+      guess << "C#{partner_place}"
     end
   end
 
   def large_ship_placement
     random_row = rand(0..3)
-    @guess << @possible_responses[random_row].shuffle.pop
+    @guess << @possible_responses[random_row].shuffle!.pop
     ship_two_coordinate_place = @possible_responses[random_row].index(@guess[2])
     ship_two_letter_row = @guess[2][0]
-    if ship_two_coordinate_place < 3
-      @guess << @possible_responses[random_row][ship_two_coordinate_place + 1]
-      @possible_responses[random_row].delete_at(ship_two_coordinate_place + 1)
-      @guess << @possible_responses[random_row][ship_two_coordinate_place + 1]
-      @possible_responses[random_row].delete_at(ship_two_coordinate_place + 1)
+    if ship_two_coordinate_place < 2
+      large_horizontal_placement(random_row, ship_two_coordinate_place)
     else
-      partner_place = coordinate_place + 1
-      if letter_row == "A"
-        guess << "B#{partner_place}"
-        guess << "C#{partner_place}"
-      elsif letter_row == "B"
-        guess << "C#{partner_place}"
-        guess << "D#{partner_place}"
-      elsif letter_row == "C"
-        guess << "B#{partner_place}"
-        guess << "A#{partner_place}"
-      else
-        guess << "C#{partner_place}"
-        guess << "B#{partner_place}"
-      end
+      large_vertical_placement(ship_two_coordinate_place, ship_two_letter_row)
     end
+  end
+
+  def large_horizontal_placement(random_row, ship_two_coordinate_place)
+    @guess << @possible_responses[random_row][ship_two_coordinate_place + 1]
+    @possible_responses[random_row].delete_at(ship_two_coordinate_place + 1)
+    @guess << @possible_responses[random_row][ship_two_coordinate_place + 1]
+    @possible_responses[random_row].delete_at(ship_two_coordinate_place + 1)
+  end
+
+  def large_vertical_placement(ship_two_coordinate_place, letter_row)
+    partner_place = ship_two_coordinate_place + 1
+    if letter_row == "A"
+      guess << "B#{partner_place}"
+      guess << "C#{partner_place}"
+    elsif letter_row == "B"
+      guess << "C#{partner_place}"
+      guess << "D#{partner_place}"
+    elsif letter_row == "C"
+      guess << "B#{partner_place}"
+      guess << "A#{partner_place}"
+    else
+      guess << "C#{partner_place}"
+      guess << "B#{partner_place}"
+    end
+
   end
 
 
