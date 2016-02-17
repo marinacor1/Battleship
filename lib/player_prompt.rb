39cbe9c60917609e358eac224b29ed0b23e891ae
marinacor1 <marinaspam1234@gmail.com>
2016-02-16 18:07:11 -0700

    tweaking error method

--- lib/player_prompt.rb
+++ lib/player_prompt.rb
@@ -1,12 +1,16 @@
 class PlayerPrompt
 
+  def initialize
+    @error = false
+  end
+
   def player_initial_ship_setup
     puts "\nI have laid out my ships on the grid. \nYou now need to layout your two ships.\nThe first ship is two units long and the second ship is three units long.\nThe grid has A1 at the top left and D4 at the bottom right.\n\nHere's an example of the grid: \n ============\n . 1 2 3 4\nA\nB\nC\nD\n\nEnter the squares for the two-unit ship:\n\nEx.A1 A2 B1 B2 B3\n\n\nChoose wisely."
     output= gets.chomp
-    player_setting_up_coordinates(output)
+    coordinates_check(output)
   end
 
-  def player_setting_up_coordinates(output)
+  def coordinates_check(output)
     possible_responses = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
     count = 0
     possible_responses.each do |coordinate|
@@ -15,14 +19,15 @@ class PlayerPrompt
       end
     end
       if count != 5
-         erroneous_response
+        erroneous_response
       end
   end
 
   def erroneous_response
-      puts "\nThis is not a valid option. Try again."
-      user_output
-   end
+    @error = true
+    puts "\nThis is not a valid option. Try again."
+    user_output
+  end
 
 
 
--- test/player_prompt_test.rb
+++ test/player_prompt_test.rb
@@ -6,16 +6,16 @@ require './lib/player_prompt'
 class PlayerPromptTest < Minitest::Test
   def test_player_prompt_creates_instance_of_PP
     pp = PlayerPrompt.new
-    pp. instance_of? PlayerPrompt
-  end
-
-  def test_pp_prompts_player_to_play
+    pp.instance_of? PlayerPrompt
   end
 
   def test_if_player_gives_invalid_input_returns_error
+    skip
+    pp = PlayerPrompt.new
+    assert pp.player_setting_up_coordinates("WW").error
   end
 
-  def test_player_prompt_allows_coordinate_info
+  def test_player_prompt_allows_only_coordinate_info
   end
 
   def test_after_play_player_is_prompted_to_end
