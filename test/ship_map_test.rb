59931b8cad02ba027f73cc910302222d7a205ab8
marinacor1 <marinaspam1234@gmail.com>
2016-02-19 00:07:26 -0700

    all sm tests passing

--- test/ship_map_test.rb
+++ test/ship_map_test.rb
@@ -9,9 +9,4 @@ class ShipMapTest < Minitest::Test
     sm.instance_of? ShipMap
   end
 
-  def test_players_coordinates_change_display_map
-  end
-
-  def test_player_can_see_their_player_boats_on_map
-  end
 end
